ad8487b01cf89796be58678392f8d56fc3105c9d
Aleksandar Pavlov <aleksandar.pavlov@vega.local>
2023-11-03 12:27:49 +0100

    validation fix

--- src/main/java/com/example/ProductsService/records/ProductApiRequest.java
+++ src/main/java/com/example/ProductsService/records/ProductApiRequest.java
@@ -5,7 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
-public record ProductApiRequest(@NotBlank(message = "Name cannot be null") @Size(min = 0, max = 30) String name, @NotBlank(message = "Description cannot be blank")  @Size(min = 0, max = 1000) String description, @NotBlank(message = "Category cannot be blank")  @Size(min = 0, max = 30)String category, @NotNull(message = "Price cannot be null") Long price, @NotNull(message = "Availability cannot be null") Boolean available, @NotNull(message = "Image cannot be null") MultipartFile image) {
+public record ProductApiRequest(@NotBlank(message = "Name cannot be null") @Length(min = 0, max = 30) String name, @NotBlank(message = "Description cannot be blank")  @Length(min = 0, max = 600) String description, @NotBlank(message = "Category cannot be blank")  @Length(min = 0, max = 30)String category, @NotNull(message = "Price cannot be null") Long price, @NotNull(message = "Availability cannot be null") Boolean available, @NotNull(message = "Image cannot be null") MultipartFile image) {
 }
--- src/main/java/com/example/ProductsService/records/ProductUpdateRequest.java
+++ src/main/java/com/example/ProductsService/records/ProductUpdateRequest.java
@@ -1,6 +1,6 @@
 package com.example.ProductsService.records;
 
-import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
 
-public record ProductUpdateRequest(@Size(min = 0, max = 50) String name ,  @Size(min = 0, max = 50) String category,  @Size(min = 0, max = 1000)String description, long price, boolean available) {
+public record ProductUpdateRequest(@Length(min = 0, max = 50) String name ,@Length(min = 0, max = 50) String category, @Length(min = 0, max = 600)String description, long price, boolean available) {
 }
